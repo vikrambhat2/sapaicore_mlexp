5c6da32e7601fe94f84c769e23c8c1a3995ed0ed
Vikram Bhat <Vikram.Bhat1@ibm.com>
2023-07-15 17:27:55 +0100

    autoai_model

--- AutoAIContainer/auto.py
+++ AutoAIContainer/auto.py
@@ -1,23 +1,23 @@
 #!/bin/python3
 from flask import Flask, jsonify, request
-import pandas as pd
 import joblib
-import pickle
+import os
+import json
+import sklearn
+import pandas as pd
 import subprocess
 import sys
 
 
-# load model
-model = joblib.load("scikit_model.pkl")
 
 app = Flask(__name__)
 #port = int(os.getenv("PORT", 9009))
-
+model = joblib.load("autoai_scikit_model.pkl")
 @app.route("/", methods=["GET"])
 def vik():
 
     return "Hello from Vikram!"
-        
+    
     
 @app.route("/v2/greet", methods=["GET"])
 def status():
@@ -26,24 +26,24 @@ def status():
         return "Flask Code: Model was not loaded."
     else:
         return "Model is loaded."
-    
+        
+
 @app.route("/v2/predict", methods=["POST"])
 def predict():
 	
     global model
     payload = request.get_json()
-    cols=[]
-    for i in range(0,3):
-        columns=model.named_steps['preprocessor'].transformers_[i][2]
-        cols = cols+columns
-    df=pd.DataFrame(payload['Input'],columns=payload['Headers'])
-    df_score=df[cols].copy()
-    result=model.predict_proba(df_score)
-    output = {'results': result[0].tolist()[0]}
+    df=pd.DataFrame(payload)
+    
+    probs=model.predict_proba(df.values).tolist()
+    preds=model.predict(df.values).tolist()
+    res =[{'prediction':preds[i], 'probability':probs[i]} for i in range(len(preds))]
+    output= {'predictions': res}
+
     return jsonify(output)
 
 
 if __name__ == "__main__":
     print("Serving Initializing")
     print("Serving Started")
-    app.run(host="0.0.0.0", debug=True, port=3000)
+    app.run(host="0.0.0.0", debug=True, port=7000)
