82529c4a248f6bb7f5a98aead99a9b101ce6bc2e
Vikram Bhat <Vikram.Bhat1@ibm.com>
2023-07-26 11:48:07 +0100

    SAP AI CORE Response updates

--- AutoAIContainer/AutoAIDocker/auto.py
+++ AutoAIContainer/AutoAIDocker/auto.py
@@ -18,7 +18,7 @@ model = joblib.load("auto-ai-pipeline.pickle")
 @app.route("/", methods=["GET"])
 def vik():
 
-    return "Hello from Vikram!"
+    return "This REST API call predicts demand response of a customer. use POST /v2/predict with your payload!"
     
     
 @app.route("/v2/greet", methods=["GET"])
@@ -27,7 +27,7 @@ def status():
     if model is None:
         return "Flask Code: Model was not loaded."
     else:
-        return "Model is loaded."
+        return "Demand Response Model is loaded."
         
 
 @app.route("/v2/predict", methods=["POST"])
@@ -35,12 +35,13 @@ def predict():
     
     global model
     payload = request.get_json()
-    df=pd.DataFrame(payload['Input'],columns=payload['Headers'])
+    
+    df=pd.DataFrame(payload['input_data'][0]['values'],columns=payload['input_data'][0]['fields'])
     
     probs=model.predict_proba(df.values).tolist()
     preds=model.predict(df.values).tolist()
     res =[{'prediction':preds[i], 'probability':probs[i]} for i in range(len(preds))]
-    output= {'predictions': res}
+    output= {'predictions': [ {"fields": ['prediction','probability'], "values": [[res[i]['prediction'], res[i]['probability']] for i in range(len(res))]}]}
 
     return jsonify(output)
 
